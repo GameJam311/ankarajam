992b8d7b0d472bdb963664a174d0594b710015f5
Mustafa <79559036+SirMustafa@users.noreply.github.com>
2023-09-02 22:19:31 +0300

    f

--- Assets/Scripts/Ui_and_buttons.cs
+++ Assets/Scripts/Ui_and_buttons.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Ui_and_buttons : MonoBehaviour
 {
@@ -9,5 +10,13 @@ public class Ui_and_buttons : MonoBehaviour
     {
         infoPanel.SetActive(false);
         Time.timeScale = 1.0f;
+    }
+    public void Starter()
+    {
+        SceneManager.LoadScene("Giris");
+    }
+    public void Exit()
+    {
+        Application.Quit();
     }
 }
