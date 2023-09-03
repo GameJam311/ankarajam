1d0d6e305062c25aa735fa082c83fd6aa6b6816a
Florianus04 <saffanduygun@gmail.com>
2023-09-03 14:48:26 +0300

    dsa

--- Assets/Scripts/KarakterKontroller.cs
+++ Assets/Scripts/KarakterKontroller.cs
@@ -142,6 +142,10 @@ public class KarakterKontroller : MonoBehaviour
         yield return new WaitForSeconds(0.8f);
         anim.SetTrigger("backidle");
     }
+    public void BolumGec()
+    {
+        MiniGameTamam = true;
+    }
     public void Recoil()// geri tepme
     {
 
